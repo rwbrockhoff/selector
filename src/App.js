7dba09cc1ae354da40fd92a61614e99d20e8e076
rwbrockhoff <brockhoffrw@gmail.com>
2023-12-10 18:07:03 -0700

    feature: add basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import {robots} from "./robots";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      robots: robots,
+      inputSearch: ""
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({inputSearch: event.target.value})
+  }
+
+  render(){
+    const {inputSearch} = this.state;
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(inputSearch.toLowerCase());
+    })
+    return (
+      <div className="App">
+        <h1>Robot Search</h1>
+        <SearchBox onSearchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
